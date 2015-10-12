f09128fad2e83227785e7d653df9ee876a33e409
Vinayak <vmvinayakmathur@gmail.com>
2015-10-12 20:33:09 +0530

    Now works with actual dataset

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import numpy as np
 import pandas as pd
 import csv
 
-#arg1 = sys.argv[1]
-#arg2 = sys.argv[2]
-#arg3 = sys.argv[3]
-#arg4 = sys.argv[4]
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+arg3 = sys.argv[3]
+arg4 = sys.argv[4]
 #Taking command line arguments 
 
 class read():
 	def __init__(self):
-		#reading csv files
-#		self.W=pd.read_csv(arg1)
-#		self.X=pd.read_csv(arg2)
-#		self.Y=pd.read_csv(arg3)
-#		self.Z=pd.read_csv(arg4)
+ 		#reading csv files
+		self.W=pd.read_csv(arg1)
+		self.X=pd.read_csv(arg2)
+		self.Y=pd.read_csv(arg3)
+		self.Z=pd.read_csv(arg4)
 		#For testing the code, you can comment the above 4 lines and uncomment the 5 lines below 
 		
 		#Right now I have taken a wine.csv file and copied and renamed it to the 4 csv file below 
@@ -25,24 +25,24 @@ class read():
 		#with interpolated values
 
 
-		self.W=pd.read_csv('id_time_vitals_train.csv')
-		self.X=pd.read_csv('id_time_labs_train.csv')
-		self.Y=pd.read_csv('id_age_train.csv')
-		self.Z=pd.read_csv('id_label_train.csv')
-		print(self.X.count())
+#		self.W=pd.read_csv('id_time_vitals_train.csv')
+#		self.X=pd.read_csv('id_time_labs_train.csv')
+#		self.Y=pd.read_csv('id_age_train.csv')
+#		self.Z=pd.read_csv('id_label_train.csv')
+		#print(self.X.count())
 
 class clean():
 	def __init__(self):
 		var=read()#reading dataset
-		print(var.Z)
+		#print(var.Z)
 		n=var.Z[((var.Z)-(var.Z).mean()).abs()<=(3*(var.Z).std())]#keep only the ones that are within +3 to -3 standard deviations in the column 'Data' and remaining to be replaced by NaN.
-                print(n)
+                #print(n)
                 self.W1=var.W.interpolate()
 		self.X1=var.X.interpolate()
 		self.Y1=var.Y.interpolate()
 		self.Z1=var.Z.interpolate()
 		#Un-comment the following line to test 
-		print(self.X1.count())
+		#print(self.X1.count())
 
 class training():
 	def __init__(self):
